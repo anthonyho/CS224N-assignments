855cd9e84deb4e5cd7f163f6465835e425bf774a
Anthony Ho <anthony.hyho@gmail.com>
2018-03-02 12:07:36 -0800

    add q1(c)(ii)

--- assignment_3/code/q1_window.py
+++ assignment_3/code/q1_window.py
@@ -37,7 +37,7 @@ class Config:
     n_word_features = 2 # Number of features for every word in the input.
     window_size = 1 # The size of the window to use.
     ### YOUR CODE HERE
-    n_window_features = 0 # The total number of features used for each window.
+    n_window_features = 6 # The total number of features used for each window.
     ### END YOUR CODE
     n_classes = 5
     dropout = 0.5
@@ -141,7 +141,10 @@ class WindowModel(NERModel):
         (Don't change the variable names)
         """
         ### YOUR CODE HERE (~3-5 lines)
-
+        self.input_placeholder = tf.placeholder(tf.int32,
+                                                shape=(None, self.config.n_window_features))
+        self.labels_placeholder = tf.placeholder(tf.int32, shape=(None,))
+        self.dropout_placeholder = tf.placeholder(tf.float32, shape=())
         ### END YOUR CODE
 
     def create_feed_dict(self, inputs_batch, labels_batch=None, dropout=1):
@@ -164,7 +167,10 @@ class WindowModel(NERModel):
             feed_dict: The feed dictionary mapping from placeholders to values.
         """
         ### YOUR CODE HERE (~5-10 lines)
-         
+        feed_dict = {self.input_placeholder: inputs_batch,
+                     self.dropout_placeholder: dropout}
+        if labels_batch is not None:
+            feed_dict[self.labels_placeholder] = labels_batch
         ### END YOUR CODE
         return feed_dict
 
@@ -185,9 +191,11 @@ class WindowModel(NERModel):
             embeddings: tf.Tensor of shape (None, n_window_features*embed_size)
         """
         ### YOUR CODE HERE (!3-5 lines)
-                                                             
-                                  
-                                                                                                                 
+        E = tf.Variable(self.pretrained_embeddings, name='E')
+        embeddings = tf.nn.embedding_lookup(E, self.input_placeholder)
+        embeddings = tf.reshape(embeddings,
+                                shape=(-1, self.config.n_window_features * self.config.embed_size),
+                                name='x')
         ### END YOUR CODE
         return embeddings
 
@@ -218,7 +226,18 @@ class WindowModel(NERModel):
         x = self.add_embedding()
         dropout_rate = self.dropout_placeholder
         ### YOUR CODE HERE (~10-20 lines)
-
+        dm = self.config.n_window_features * self.config.embed_size
+        W = tf.get_variable('W', shape=(dm, self.config.hidden_size),
+                            initializer=tf.contrib.layers.xavier_initializer())
+        b1 = tf.get_variable('b1', shape=(1, self.config.hidden_size),
+                             initializer=tf.zeros_initializer())
+        U = tf.get_variable('U', shape=(self.config.hidden_size, self.config.n_classes),
+                            initializer=tf.contrib.layers.xavier_initializer())
+        b2 = tf.get_variable('b2', shape=(1, self.config.n_classes),
+                             initializer=tf.zeros_initializer())
+        h = tf.nn.relu(tf.matmul(x, W) + b1, name='h')
+        h_drop = tf.nn.dropout(h, 1 - dropout_rate, name='h_drop')
+        pred = tf.matmul(h_drop, U) + b2
         ### END YOUR CODE
         return pred
 
@@ -236,7 +255,9 @@ class WindowModel(NERModel):
             loss: A 0-d tensor (scalar)
         """
         ### YOUR CODE HERE (~2-5 lines)
-                                   
+        loss = tf.nn.softmax_cross_entropy_with_logits(labels=self.labels_placeholder,
+                                                       logits=pred)
+        loss = tf.reduce_mean(loss)
         ### END YOUR CODE
         return loss
 
@@ -260,7 +281,8 @@ class WindowModel(NERModel):
             train_op: The Op for training.
         """
         ### YOUR CODE HERE (~1-2 lines)
-
+        opt = tf.train.AdamOptimizer(learning_rate=self.config.lr)
+        train_op = opt.minimize(loss)
         ### END YOUR CODE
         return train_op
 
